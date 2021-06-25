300cff8a6624334cfc9bdf4d23e89877e7d537eb
Robson Ferreira <tech.rob@icloud.com>
2021-06-25 19:05:05 -0400

    second commit

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ const app = express();
 
 /* GET home page. */
 
+router.get("/", (req, res, next) => res.render("index", { title: "Express" }));
+
+router.get("/helloworld", (req, res) =>
+  res.render("helloworld", { title: "Hello World from Express!" })
+);
+
 router.get("/userlist", (req, res) => {
   let db = req.db;
   let collection = db.get("usercollection");
@@ -15,10 +21,28 @@ router.get("/userlist", (req, res) => {
   });
 });
 
-router.get("/", (req, res, next) => res.render("index", { title: "Express" }));
-
-router.get("/hellofromexpress", (req, res) =>
-  res.render("hellofromexpress", { title: "Hello from Express!" })
+router.get("/newuser", (req, res) =>
+  res.render("newuser", { title: "Add new user" })
 );
+
+router.post("/adduser", (req, res) => {
+  const db = req.db;
+  const userName = req.body.username;
+  const userEmail = req.body.userEmail;
+
+  const collection = db.get("usercollection");
+
+  collection.insert(
+    {
+      username: userName,
+      email: userEmail,
+    },
+    (err, doc) => {
+      err
+        ? res.send("there was a problem adding data to the database.")
+        : res.redirect("userlist");
+    }
+  );
+});
 
 module.exports = router;
